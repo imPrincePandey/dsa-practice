e4f11d0df2433170da2f86f91dfe64a45246e450
Prince Kumar <imprincepandey1912@gmail.com>
2025-09-19 23:26:13 +0530

    feat: add how to declare ArrayList and add element

---
+++ src/arrayList/ArrayListPractice1.java
@@ -0,0 +1,26 @@
+package arrayList;
+import java.util.ArrayList;
+
+public class ArrayListPractice1 {
+
+	public static void main(String[] args) {
+		// declaration of ArrayList
+		ArrayList<Integer> list = new ArrayList<>();
+		
+		// Adding Element
+		
+		list.add(10);
+		list.add(12);
+		list.add(34);
+		list.add(56);
+		list.add(45);
+		list.add(50);
+		
+		// Printing entire list
+        System.out.println("ArrayList: " + list);
+		
+		
+
+	}
+
+}
